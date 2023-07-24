141156a74c3195f5d1db8e8b54c021a6e406bda9
ydeng24 <yiming-deng@outlook.com>
2023-07-24 16:09:22 -0500

    checkin 3.0

--- src/command/MouseModeExecution.java
+++ src/command/MouseModeExecution.java
@@ -1,51 +1,6 @@
-// package command;
-
-// // ydeng24@depaul.edu
-
-// import model.Point;
-// import model.MouseMode;
-// import model.Proxy.SelectedOutlineProxy;
-// import model.interfaces.IApplicationState;
-// import model.interfaces.ICommand;
-// import model.interfaces.ISelectedShapeOutline;
-// import view.interfaces.IPaintCanvas;
-
-// // Command Pattern
-// public class MouseModeExecution {
-
-//     public static void clickedMode(Point startPoint, Point endPoint,
-//                                    IPaintCanvas paintCanvas, IApplicationState appState) {
-//         ICommand command = null;
-//         SelectedOutlineProxy selectShapeProxy = null;
-//         SelectedOutlineProxy moveShapeProxy = null;
-
-//         if (appState.getActiveStartAndEndPointMode() == MouseMode.DRAW) {
-//             command = new DrawCommand(startPoint, endPoint, paintCanvas, appState);
-//             command.run();
-//         }
-        
-//         // Proxy Pattern
-//         else if (appState.getActiveStartAndEndPointMode() == MouseMode.SELECT) {
-//             command = new SelectCommand(startPoint, endPoint, paintCanvas);
-//             command.run();
-//             selectShapeProxy = new SelectedOutlineProxy(command);
-//             printShapeOutline(selectShapeProxy);
-//         }
-//         else if (appState.getActiveStartAndEndPointMode() == MouseMode.MOVE) {
-//             command = new MoveCommand(startPoint, endPoint, paintCanvas);
-//             command.run();
-//             moveShapeProxy = new SelectedOutlineProxy(command);
-//             printShapeOutline(moveShapeProxy);
-//         }
-//     }
-
-//     // Proxy pattern
-//     public static void printShapeOutline(ISelectedShapeOutline outlineOperation) {
-//         outlineOperation.shapeOutline();
-//     }
-// }
-
 package command;
+
+// ydeng24@depaul.edu
 
 import model.Point;
 import model.MouseMode;
@@ -67,7 +22,6 @@ public class MouseModeExecution {
 
         command.run();
 
-        // Use Proxy pattern only when we are either in Select mode or Move mode.
         if (appState.getActiveStartAndEndPointMode() == MouseMode.SELECT
                 || appState.getActiveStartAndEndPointMode() == MouseMode.MOVE) {
             SelectedOutlineProxy shapeProxy = new SelectedOutlineProxy(command);
@@ -75,7 +29,6 @@ public class MouseModeExecution {
         }
     }
 
-    // A separate function to return command based on the mode.
     private static ICommand getCommand(MouseMode mode, Point startPoint, Point endPoint, IPaintCanvas paintCanvas, IApplicationState appState) {
         ICommand command = null;
         switch (mode) {
