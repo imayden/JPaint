d5b0360dbc7f997fca2d5176f36b13381e8aa8f4
ydeng24 <yiming-deng@outlook.com>
2023-08-08 22:24:39 -0500

    Check-In 4.1

--- src/pattern/command/Delete.java
+++ src/pattern/command/Delete.java
@@ -2,68 +2,29 @@ package pattern.command;
 
 // ydeng24@depaul.edu
 
-import model.ExistingShape;
-import model.interfaces.*;
 import pattern.command.interfaces.IUndoable;
-import pattern.factory.interfaces.IShape;
-import pattern.proxy.OutlineHandler;
-import view.gui.UpdateCanvas;
-import view.interfaces.IPaintCanvas;
-import java.util.LinkedList;
 
-public class Delete implements IClipboard, IActiveShape, IUndoable {
+// Command class that uses DeletionHandler to execute the delete operation
+public class Delete implements IUndoable {
 
-    private LinkedList<IShape> shapesToDelete;
-    private IPaintCanvas paintCanvas = null;
-    private OutlineHandler activeShapeOutline;
-
-    public Delete() {
-        shapesToDelete = new LinkedList<>();
-        ExistingShape.shapeList.stream()
-        .filter(activeShape::contains)
-        .forEach(selectedShape -> {
-            if (paintCanvas == null)
-                paintCanvas = selectedShape.getPaintCanvas();
-            shapesToDelete.add(selectedShape);
-        });
-    }
+    private DeletionHandler handler;
     
+    public Delete() {
+        handler = new DeletionHandler();
+    }
 
     public void execute() {
-        shapesToDelete.forEach(shape -> {
-            activeShape.remove(shape);
-            ExistingShape.shapeList.remove(shape);
-        });
-    
-        if (paintCanvas != null) {
-            UpdateCanvas.update(paintCanvas);
-        }
-        CommandInvoker.add(this);
+        handler.execute();
     }
 
     @Override
     public void undo() {
-        shapesToDelete.forEach(shape -> {
-            activeShape.add(shape);
-            ExistingShape.shapeList.add(shape);
-        });
-    
-        if (paintCanvas != null) {
-            UpdateCanvas.update(paintCanvas);
-            activeShapeOutline = new OutlineHandler();
-            activeShapeOutline.outline();
-        }
+        handler.undo();
     }
 
     @Override
     public void redo() {
-        shapesToDelete.forEach(shape -> {
-            activeShape.remove(shape);
-            ExistingShape.shapeList.remove(shape);
-        });
-    
-        if (paintCanvas != null) {
-            UpdateCanvas.update(paintCanvas);
-        }
+        handler.redo();
     }
 }
+
